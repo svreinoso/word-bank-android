27e3b367600ad4d309d205015e85068150af9d81
Samuel Reinoso <sreinoso@intellisys.com.do>
2019-02-26 16:47:39 -0400

    change status numbers

--- app/src/main/java/com/example/samuel/wordbank/data/WordStatus.java
+++ app/src/main/java/com/example/samuel/wordbank/data/WordStatus.java
@@ -1,7 +1,7 @@
 package com.example.samuel.wordbank.data;
 
 public class WordStatus {
-    public static final int LEARNED = 1;
-    public static final int ADDED = 2;
-    public static final int LEARNING  = 3;
+    public static final int ADDED = 1;
+    public static final int LEARNING  = 2;
+    public static final int LEARNED = 3;
 }
